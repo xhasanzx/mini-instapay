7e5272f07a8e1b6d1f55267e0d395190cf8122cc
xhasanzx <hasandarweshzx@gmail.com>
2025-05-21 16:48:17 +0300

    other link?

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -21,7 +21,14 @@ const LoginForm = () => {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
-      });
+      });      
+      if (!response.ok) {
+        response = await fetch(`http://192.168.58.2:30001/user/login/`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username, password }),
+        });
+      }
       const data = await response.json();
       if (!response.ok) {
         setError(data.error || "Login failed");
